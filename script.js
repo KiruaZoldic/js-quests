c18d04850029fbb1398417cfc534cb70b941369c
Robin <duytschaeverrobin@gmail.com>
2025-04-03 11:58:07 +0200

    JS quest n° 7bis done

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-// Déclaration et définition de trois variables avec une valeur numérique
-const a = 3;
-const b = 5;
-const c = 9;
+const mysteriousString = "iu@zfiz)!uzqzf!snoi??alutargnocze&gfuzyafzygfzmgfu%f";
+console.log("step 0 : ", mysteriousString);
 
-// Vérification des conditions
-if (a === b && b === c) {
-	console.log("Les trois variables sont identiques");
-} else if (a === b || a === c || b === c) {
-	console.log("Deux variables sont identiques");
-} else {
-	console.log("Toutes les variables sont différentes");
-}
+// step1 : split the mysterious string into an array, so that each letter becomes an item
+const step1 = mysteriousString.split("");
+console.log("step 1 : ", step1);
+
+// step2 : get a slice of the array : take elements from the 15th included to the 32nd excluded
+const step2 = step1.slice(15, 32);
+console.log("step 2 : ", step2);
+
+// step3 : Splice the array to replace 2 elements from index 5 with only one element : the letter 't'
+const step3 = step2.slice(); // making a copy
+step3.splice(5, 2, "t");
+console.log("step 3 : ", step3);
+
+// step4 : reverse the array
+const step4 = step3.reverse();
+console.log("step 4 : ", step4);
+
+// step5 : each element of the array back into a string
+const step5 = step4.join("");
+console.log("step 5 : ", step5);
